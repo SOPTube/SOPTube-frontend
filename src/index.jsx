6fa6a4928e92aa787bd7af62dc7e6b474d0387f4
nahee-park <jessica396@ewhain.net>
2022-05-13 22:13:22 +0900

    [add] index.css

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyle from './styles/global';
 import { ThemeProvider } from 'styled-components';
+import './index.css';
 import theme from './styles/theme';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
