3dbc87f4f92dbc34a0eb77d254c5fac93751ba28
nahee-park <jessica396@ewhain.net>
2022-05-13 21:55:14 +0900

    [init] lint 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div>
-      메인페이지입니다.
-    </div>
-  );
+  return <div>메인페이지입니다.</div>;
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import GlobalStyle from './style/global';
-
+import GlobalStyle from './styles/global';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyle/>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
 );
-
