9e32d529da077095465be9e01eede1d2303e5184
nahee-park <jessica396@ewhain.net>
2022-05-26 07:03:03 +0900

    refact: 더미데이터 정리

---
+++ src/mock-data/index.js
@@ -0,0 +1,123 @@
+// 더미데이터, 추후 서버 연결
+export const mainVideoData = [
+  {
+    videoId: '1',
+    videoThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    duration: '00:16',
+    userThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    title: '고야야야야양이잉야이이',
+    user: '나히바나나',
+    views: 10200,
+    timeAgo: '1개월 전',
+  },
+  {
+    videoId: '2',
+    videoThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    duration: '00:16',
+    userThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    title: '고야야야야양이잉야이이',
+    user: '나히바나나',
+    views: 10200,
+    timeAgo: '1개월 전',
+  },
+  {
+    videoId: '3',
+    videoThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    duration: '00:16',
+    userThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    title: '고야야야야양이잉야이이',
+    user: '나히바나나',
+    views: 10200,
+    timeAgo: '1개월 전',
+  },
+  {
+    videoId: '4',
+    videoThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    duration: '00:16',
+    userThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    title: '고야야야야양이잉야이이',
+    user: '나히바나나',
+    views: 10200,
+    timeAgo: '1개월 전',
+  },
+  {
+    videoId: '5',
+    videoThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    duration: '00:16',
+    userThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    title: '고야야야야양이잉야이이',
+    user: '나히바나나',
+    views: 10200,
+    timeAgo: '1개월 전',
+  },
+  {
+    videoId: '6',
+    videoThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    duration: '00:16',
+    userThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    title: '고야야야야양이잉야이이',
+    user: '나히바나나',
+    views: 10200,
+    timeAgo: '1개월 전',
+  },
+  {
+    videoId: '7',
+    videoThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    duration: '00:16',
+    userThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    title: '고야야야야양이잉야이이',
+    user: '나히바나나',
+    views: 10200,
+    timeAgo: '1개월 전',
+  },
+  {
+    videoId: '8',
+    videoThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    duration: '00:16',
+    userThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    title: '고야야야야양이잉야이이',
+    user: '나히바나나',
+    views: 10200,
+    timeAgo: '1개월 전',
+  },
+  {
+    videoId: '9',
+    videoThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    duration: '00:16',
+    userThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    title: '고야야야야양이잉야이이',
+    user: '나히바나나',
+    views: 10200,
+    timeAgo: '1개월 전',
+  },
+  {
+    videoId: '10',
+    videoThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    duration: '00:16',
+    userThumbnail:
+      'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
+    title: '고야야야야양이잉야이이',
+    user: '나히바나나',
+    views: 10200,
+    timeAgo: '1개월 전',
+  },
+];
--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -2,135 +2,15 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import MainTagList from '../component/main/MainTagList';
 import MainVideo from '../component/main/MainVideo';
+import { mainVideoData } from '../mock-data';
 
 function Main() {
   const [currentType, setCurrentType] = useState('동물');
-  const [videoData, setVideoData] = useState([
-    {
-      videoId: '1',
-      videoThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      duration: '00:16',
-      userThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      title: '고야야야야양이잉야이이',
-      user: '나히바나나',
-      views: 10200,
-      timeAgo: '1개월 전',
-    },
-    {
-      videoId: '2',
-      videoThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      duration: '00:16',
-      userThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      title: '고야야야야양이잉야이이',
-      user: '나히바나나',
-      views: 10200,
-      timeAgo: '1개월 전',
-    },
-    {
-      videoId: '3',
-      videoThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      duration: '00:16',
-      userThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      title: '고야야야야양이잉야이이',
-      user: '나히바나나',
-      views: 10200,
-      timeAgo: '1개월 전',
-    },
-    {
-      videoId: '4',
-      videoThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      duration: '00:16',
-      userThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      title: '고야야야야양이잉야이이',
-      user: '나히바나나',
-      views: 10200,
-      timeAgo: '1개월 전',
-    },
-    {
-      videoId: '5',
-      videoThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      duration: '00:16',
-      userThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      title: '고야야야야양이잉야이이',
-      user: '나히바나나',
-      views: 10200,
-      timeAgo: '1개월 전',
-    },
-    {
-      videoId: '6',
-      videoThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      duration: '00:16',
-      userThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      title: '고야야야야양이잉야이이',
-      user: '나히바나나',
-      views: 10200,
-      timeAgo: '1개월 전',
-    },
-    {
-      videoId: '7',
-      videoThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      duration: '00:16',
-      userThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      title: '고야야야야양이잉야이이',
-      user: '나히바나나',
-      views: 10200,
-      timeAgo: '1개월 전',
-    },
-    {
-      videoId: '8',
-      videoThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      duration: '00:16',
-      userThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      title: '고야야야야양이잉야이이',
-      user: '나히바나나',
-      views: 10200,
-      timeAgo: '1개월 전',
-    },
-    {
-      videoId: '9',
-      videoThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      duration: '00:16',
-      userThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      title: '고야야야야양이잉야이이',
-      user: '나히바나나',
-      views: 10200,
-      timeAgo: '1개월 전',
-    },
-    {
-      videoId: '10',
-      videoThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      duration: '00:16',
-      userThumbnail:
-        'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCabcw%2Fbtrrdmj0A58%2F1YkOil23N3sFbQsrY61qpK%2Fimg.jpg',
-      title: '고야야야야양이잉야이이',
-      user: '나히바나나',
-      views: 10200,
-      timeAgo: '1개월 전',
-    },
-  ]);
+
   return (
     <Styled.Root>
       <MainTagList setCurrentType={setCurrentType} />
-      <MainVideo data={videoData} />
+      <MainVideo data={mainVideoData} />
     </Styled.Root>
   );
 }
