2737927ada8595db7ebed6fd69ba6752b0152cea
Chedda98 <twa05157@gmail.com>
2022-05-27 23:56:50 +0900

    Feat: 헤더 완성

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,31 +1,31 @@
-import React from "react";
-import styled from "styled-components";
-import Logo from "../assets/assets_header/img_logo.png";
-import Notice from "../assets/assets_header/icn_notice.png";
-import Menubar from "../assets/assets_header/icn_menu.png";
-import Search from "../assets/assets_header/icn_search.png";
-import Totalmenu from "../assets/assets_header/icn_totalmenu.png";
-import Camera from "../assets/assets_header/icn_camera.png";
-import Profile from "../assets/assets_header/profile.png";
+import React from 'react';
+import styled from 'styled-components';
+import Logo from '../assets/assets_header/img_logo.png';
+import Notice from '../assets/assets_header/icn_notice.png';
+import Menubar from '../assets/assets_header/icn_menu.png';
+import Search from '../assets/assets_header/icn_search.png';
+import Totalmenu from '../assets/assets_header/icn_totalmenu.png';
+import Camera from '../assets/assets_header/icn_camera.png';
+import Profile from '../assets/assets_header/profile.png';
 
 const Header = () => {
   return (
     <Styled.Wrapper>
       <Styled.Set>
-        <Styled.Button src={Menubar} />
-        <Styled.Button src={Logo} />
+        <Button src={Menubar} />
+        <StyledLogo src={Logo} />
       </Styled.Set>
 
       <Styled.Search>
         <Styled.Input type="text" placeholder="검색" />
-        <Styled.Button src={Search} />
+        <Button src={Search} />
       </Styled.Search>
 
       <Styled.Set>
-        <Styled.Button src={Camera} />
-        <Styled.Button src={Totalmenu} />
-        <Styled.Button src={Notice} />
-        <Styled.Button src={Profile} />
+        <Button src={Camera} />
+        <Button src={Totalmenu} />
+        <Button src={Notice} />
+        <Button src={Profile} />
       </Styled.Set>
     </Styled.Wrapper>
   );
@@ -33,50 +33,50 @@ const Header = () => {
 
 const Styled = {
   Wrapper: styled.div`
-  display: flex;
-  position: fixed;
-  top: 0;
-  width: 97%;
-  background-color: white;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0px 20px;
-  height: 70px;
-  z-index: 1;
+    display: flex;
+    position: fixed;
+    top: 0;
+    width: 97%;
+    background-color: white;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0px 20px;
+    height: 56px;
+    z-index: 1;
 
-  @media (max-width: 800px) {
-    justify-content: space-around;
-  }
+    @media (max-width: 800px) {
+      justify-content: space-around;
+    }
 
-  .icon {
-    width: 24px;
-    height: 24px;
-    cursor: pointer;
-  }
+    .icon {
+      width: 24px;
+      height: 24px;
+      cursor: pointer;
+    }
   `,
   Set: styled.div`
-  display: flex;
-  gap: 25px;
+    display: flex;
+    gap: 25px;
+    align-items: center;
   `,
-  Button: styled.img``,
   Search: styled.div`
     display: flex;
-  flex-direction: row;
-  align-items: center;
-  width: 50%;
-  margin: 0px 25px;
-  .search-button {
-    width: 24px;
-    height: 24px;
-    padding: 2px 20px;
-    border: 1px solid #d3d3d3;
-    background-color: #f8f8f8;
-    cursor: pointer;
-  }
-  @media (max-width: 800px) {
-    display: none;
-  }
+    flex-direction: row;
+    align-items: center;
+    width: 50%;
+    margin: 0px 25px;
+    .search-button {
+      width: 24px;
+      height: 24px;
+      padding: 2px 20px;
+      border: 1px solid #d3d3d3;
+      background-color: #f8f8f8;
+      cursor: pointer;
+    }
+    @media (max-width: 800px) {
+      display: none;
+    }
   `,
   Input: styled.input`
     flex: 1;
@@ -87,7 +87,17 @@ const Styled = {
     ::placeholder {
       color: gray;
     }
-  `
+  `,
 };
+
+const Button = styled.img`
+  width: 24px;
+  height: 24px;
+`;
+
+const StyledLogo = styled.img`
+  width: 100px;
+  height: 40px;
+`;
 
 export default Header;
