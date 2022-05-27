a76a44c82f81275214d50312852c97f850ce8fd5
yunsun99 <79686751+yunsun99@users.noreply.github.com>
2022-05-27 12:40:38 +0900

    feat: Sidebar 컴포넌트 상세 디자인 및 이미지 적용

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,3 +1,17 @@
-const theme = {};
-
+const color = {
+  main_red: '#E93222',
+  point_blue: '#295ECC',
+  black: '#000000',
+  white: '#FFFFFF',
+  alpha_black: `rgba(0, 0, 0, 0.8)`,
+  dark_grey: '#030403',
+  grey_01: '#606060',
+  default_grey: '#C4C4C4',
+  light_grey_01: '#D3D3D3',
+  light_grey_02: '#ECECEC',
+  light_grey_03: '#F9F9F9',
+  light_grey_04: '#F8F9F8',
+  light_grey_05: '#E3E3E3',
+};
+const theme = { color };
 export default theme;
